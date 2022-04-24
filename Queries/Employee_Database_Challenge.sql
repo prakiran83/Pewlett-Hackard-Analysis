fd609dea5f71f9310deec21cb28ed4d1e7139d99
prakiran83 <100887673+prakiran83@users.noreply.github.com>
2022-04-24 12:57:54 -0500

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,10 +1,47 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--Deliverable 1
+--creating retirement_titles.csv
+SELECT e.emp_no, 
+	   e.first_name, 
+	   t.title, 
+	   t.from_date, 
+	   t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+-- Creating unique_titles.csv
+SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no,
+			retirement_titles.first_name, 
+			retirement_titles.title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY retirement_titles.emp_no, retirement_titles.to_date DESC;
+
+-- Creating retiring_titles.csv
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT (ut.emp_no) DESC;
+
+
+--Deliverable 2 
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+    INNER JOIN dept_emp as de
+    ON (e.emp_no = de.emp_no)
+    INNER JOIN titles as t
+    ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
